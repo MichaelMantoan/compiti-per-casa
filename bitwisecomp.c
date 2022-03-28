c3b1ba9661dff34201b8e04c5ad973d2fa9ba543
MichaelMantoan <98811594+MichaelMantoan@users.noreply.github.com>
2022-03-28 13:33:35 -0400

    operatore ternario

--- bitwisecomp.c
+++ bitwisecomp.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-int num1=9;
-int num2=3;
-int ris1,ris2,ris3;
-ris1=num1|num2;
-ris2=num1&num2;
-ris3=num1^num2;
-printf("%d,%d,%d",ris1,ris2,ris3);
-//output ottenuto sono stati stampati questi risultati 11,1,10
+int a;
+scanf("%d", &a);
+a=(a>0)?printf("numero positivo"):printf("numero negativo");
+//
+
 
 return 0;
 
